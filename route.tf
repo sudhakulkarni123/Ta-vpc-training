e0d89a216c24e9c61efc8b8af1c3edf979bc21df
sudhakulkarni123 <sudha.kulkarni@cloudreach.com>
2022-07-21 14:39:04 +0530

    route connectiong to instance2

--- route.tf
+++ route.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "internet_route_tbl" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_route_tbl.id
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
   tags = {
